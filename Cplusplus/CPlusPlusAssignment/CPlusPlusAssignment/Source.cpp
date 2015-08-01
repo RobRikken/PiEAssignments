f02869e8cff63d6312aa81edbd8fdc5afdec87ef
Rob Rikken <rbrikken@gmail.com>
2015-08-01 21:08:16 +0200

    Added C++ part of the assignment.

---
+++ Cplusplus/CPlusPlusAssignment/CPlusPlusAssignment/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+	int NPrimeNumbers;
+
+	cout	<< "Please input the number of prime numbers you want to calculate." << endl 
+			<< "Type a integer number and press enter:" << endl;
+	
+	cin >> NPrimeNumbers;
+
+	return EXIT_SUCCESS
+}
